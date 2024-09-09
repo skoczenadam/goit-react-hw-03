29d1a7a60a4e5235328dc97b6be97c87ce4809c7
Adam Skoczeń <adamsk91@wp.pl>
2024-09-09 17:24:21 +0200

    3211111

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,26 +4,26 @@ import SearchBox from "./SearchBox";
 import ContactList from "./ContactList";
 import { useState, useEffect } from "react";
 
-const phoneBook = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
+// const phoneBook = [
+//   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+//   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+//   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+//   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+// ];
 
 const App = () => {
-  // const [contacts, setContacts] = useState(() => {
-  //   try {
-  //     const contactsFromLocalStore = localStorage.getItem("phonebook");
-  //     if (contactsFromLocalStore !== null) {
-  //       return JSON.parse(contactsFromLocalStore);
-  //     }
-  //     return {};
-  //   } catch (e) {
-  //     return phoneBook;
-  //   }
-  // });
-  const [contacts, setContacts] = useState(phoneBook);
+  const [contacts, setContacts] = useState(() => {
+    try {
+      const contactsFromLocalStore = localStorage.getItem("phonebook");
+      if (contactsFromLocalStore !== null) {
+        return JSON.parse(contactsFromLocalStore);
+      }
+      return {};
+    } catch (e) {
+      return {};
+    }
+  });
+  // const [contacts, setContacts] = useState(phoneBook);
   const [filter, setFilter] = useState("");
 
   const handleFilterChange = (e) => {
