48c207d1ee415385b94c01b35b348c759884262f
Adam Skoczeń <adamsk91@wp.pl>
2024-09-09 17:31:37 +0200

    change name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,17 @@ import SearchBox from "./SearchBox";
 import ContactList from "./ContactList";
 import { useState, useEffect } from "react";
 
-const phoneBook = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
+// const phoneBook = [
+//   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+//   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+//   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+//   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+// ];
 
 const App = () => {
   const [contacts, setContacts] = useState(() => {
     try {
-      const contactsFromLocalStore = localStorage.getItem("phonebook1");
+      const contactsFromLocalStore = localStorage.getItem("phonebook");
       if (contactsFromLocalStore !== null) {
         return JSON.parse(contactsFromLocalStore);
       }
@@ -43,7 +43,7 @@ const App = () => {
   };
 
   useEffect(() => {
-    localStorage.setItem("phonebook1", JSON.stringify(contacts));
+    localStorage.setItem("phonebook", JSON.stringify(contacts));
   }, [contacts]);
 
   return (
