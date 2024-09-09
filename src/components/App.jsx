c73750bfef7b2d33cccfcc1e5bc4932862e58960
Adam Skoczeń <adamsk91@wp.pl>
2024-09-09 17:04:57 +0200

    phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,12 @@ import SearchBox from "./SearchBox";
 import ContactList from "./ContactList";
 import { useState, useEffect } from "react";
 
-// const phoneBook = [
-//   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-//   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-//   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-//   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-// ];
+const phoneBook = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
 
 const App = () => {
   const [contacts, setContacts] = useState(() => {
@@ -20,7 +20,7 @@ const App = () => {
       }
       return {};
     } catch (e) {
-      return [{}];
+      return phoneBook;
     }
   });
   // const [contacts, setContacts] = useState(phoneBook);
